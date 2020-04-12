7cee6e9ce0de3bb53ac5240f69023c0fd4e68f16
Bartek Jurczewski <bartek.jurczewski@gmail.com>
2020-04-12 23:30:25 +0200

    Recipe View

--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -33,6 +33,7 @@ export default class Recipe {
     parseIngredietns() {
         const unitsLong = ['tablespoons', 'tablespoon', 'ounces', 'ounce', 'teaspoons', 'teaspoon', 'cups', 'pounds'];
         const unitsShort = ['tbsp', 'tbsp', 'oz', 'oz', 'tsp', 'tsp', 'cup', 'pound'];
+        const units = [...unitsShort, 'kg', 'g'];
 
         const newIngredients = this.ingredients.map(el => {
             // 1) Uniform units
@@ -46,7 +47,7 @@ export default class Recipe {
 
             // 3) Parse ingredients into count, unit and ingredients
             const arrIng = ingredient.split(' ');
-            const unitIndex = arrIng.findIndex(el2 => unitsShort.includes(el2));
+            const unitIndex = arrIng.findIndex(el2 => units.includes(el2));
 
             let objIng;
             if (unitIndex > -1) {
@@ -72,7 +73,7 @@ export default class Recipe {
                 objIng = {
                     count: parseInt(arrIng[0], 10),
                     unit: '',
-                    ingredient: arrIng.slice(1)
+                    ingredient: arrIng.slice(1).join(' ')
                 };
             } else if (unitIndex === -1) {
                 // There is NO unit and NO nmber in 1st position
--- src/js/view/base.js
+++ src/js/view/base.js
@@ -4,6 +4,7 @@ export const elements = {
     searchRes: document.querySelector('.results'),
     searchResList: document.querySelector('.results__list'),
     serachResPages: document.querySelector('.results__pages'),
+    recipe: document.querySelector('.recipe'),
 };
 
 export const elementStrings = {
